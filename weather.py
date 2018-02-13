f9605a5a0c1d3d6167ff0edfddf2e5ef51c4a93c
ChristopherRush <chris.uclan@gmail.com>
2018-02-13 21:00:31 +0000

    i2c check

--- weather.py
+++ weather.py
@@ -13,33 +13,44 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
+import smbus
 import Adafruit_BMP.BMP085 as BMP085 #Works for both the BMP085 and BMP180 sensors
 import Adafruit_DHT # this library works for DHT11 DHT22 and AM2302 sensors
+
+bus = smbus.SMBus(1)
 
 from flask import Flask, render_template
 
 bmp_sensor = BMP085.BMP085()
 dh22_sensor = Adafruit_DHT.DHT22
 
+bmp_device = 119
 pin = 4 #DHT22 data pin on the raspberry pi
 
 #temp = sensor.read_temperature()
 #pressure = sensor.read_pressure()
 #altitude = sensor.read_altitude()
 
+
+
+
+
 app = Flask(__name__)
 
 @app.route('/') # this tells the program what url triggers the function when a request is made
 def index():
+
         humidity, temperature = Adafruit_DHT.read_retry(dh22_sensor, pin) #get the values from the sensor
         humidity ='{:.2f}'.format(humidity) #convert value to two decimal places
         temperature ='{:.1f}'.format(temperature) #convert value to one decimal place
 
-        temp = bmp_sensor.read_temperature() #read the temperature from the BMP sensor in celcius
-        pressure = bmp_sensor.read_pressure() #read the pressure from the BMP sensor
-        altitude = bmp_sensor.read_altitude() #read teh altitude value from the BMP sensor in meters
-        altitude = '{:.2f}'.format(altitude) #convert the altitude value to two decimal places
+        bus.read_byte(device)
+        if hex(device) == 0x77:
+            print (hex(device))
+            temp = bmp_sensor.read_temperature() #read the temperature from the BMP sensor in celcius
+            pressure = bmp_sensor.read_pressure() #read the pressure from the BMP sensor
+            altitude = bmp_sensor.read_altitude() #read teh altitude value from the BMP sensor in meters
+            altitude = '{:.2f}'.format(altitude) #convert the altitude value to two decimal places
 
         #variables to pass through to the web page
         templateData = {
