cc2047f1135bc572032c24f345da570f0681d832
ChristopherRush <chris.uclan@gmail.com>
2018-02-14 11:29:23 +0000

    bash

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Install BMP Adafruit library
-
 git clone https://github.com/adafruit/Adafruit_Python_BMP.git
 cd Adafruit_Python_BMP
 python setup.py install
 
-# Install DHT22 Adafruit library
+cd..
 
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_BMP
 python setup.py install
+
+cd..
 
 git clone https://github.com/ChristopherRush/weather.git
