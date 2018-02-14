ce2cbd484d9d857869a9fb54336243d5827fb71b
ChristopherRush <chris.uclan@gmail.com>
2018-02-14 14:54:01 +0000

    value

--- weather.py
+++ weather.py
@@ -60,8 +60,8 @@ def index():
 
 
     try:
-        if bus.read_byte(bmp_device): #check to see if the BMP sensor is attached decimal 119 hex 0x77 address
-            print "first"
+        if bus.read_byte(0x77): #check to see if the BMP sensor is attached decimal 119 hex 0x77 address
+
             temp = bmp_sensor.read_temperature() #read the temperature from the BMP sensor in celcius
             pressure = bmp_sensor.read_pressure() #read the pressure from the BMP sensor
             altitude = bmp_sensor.read_altitude() #read teh altitude value from the BMP sensor in meters
