55e8d5d5e57ab6596e4dc0951dc9b7ba3a829448
ChristopherRush <chris.uclan@gmail.com>
2018-03-21 12:28:05 +0000

    run air quality script separate

--- weather.py
+++ weather.py
@@ -19,9 +19,12 @@ import Adafruit_DHT # this library works for DHT11 DHT22 and AM2302 sensors
 import bme680 # import bme680 library
 import time
 import subprocess
+from gas_burin.py import air_quality_score
 
 subprocess.call(["echo", "hostname -I"])
 
+print"Waiting 5 mins for air quality burn in data"
+time.sleep(300)
 
 
 sensor = bme680.BME680() #create bme680 object
@@ -35,40 +38,6 @@ sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
 sensor.set_gas_heater_temperature(320)
 sensor.set_gas_heater_duration(150)
 sensor.select_gas_heater_profile(0)
-
-start_time = time.time()
-curr_time = time.time()
-burn_in_time = 120
-
-
-
-burn_in_data = []
-
-try:
-    # Collect gas resistance burn-in values, then use the average
-    # of the last 50 values to set the upper limit for calculating
-    # gas_baseline.
-    print("Collecting gas resistance burn-in data for 5 mins\n")
-    while curr_time - start_time < burn_in_time:
-        curr_time = time.time()
-        if sensor.get_sensor_data() and sensor.data.heat_stable:
-            gas = sensor.data.gas_resistance
-            burn_in_data.append(gas)
-            print("Gas: {0} Ohms".format(gas))
-            time.sleep(1)
-
-    gas_baseline = sum(burn_in_data[-50:]) / 50.0
-
-    # Set the humidity baseline to 40%, an optimal indoor humidity.
-    hum_baseline = 40.0
-
-    # This sets the balance between humidity and gas reading in the
-    # calculation of air_quality_score (25:75, humidity:gas)
-    hum_weighting = 0.25
-
-    print("Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n".format(gas_baseline, hum_baseline))
-except:
-    pass
 
 bus = smbus.SMBus(1)
 
@@ -94,42 +63,16 @@ pin = 4 #DHT22 data pin on the raspberry pi
 #pressure = sensor.read_pressure()
 #altitude = sensor.read_altitude()
 
-
-
 app = Flask(__name__)
 
 @app.route('/') # this tells the program what url triggers the function when a request is made
 def index():
     try:
-        if sensor.get_sensor_data() and sensor.data.heat_stable:
-            gas = sensor.data.gas_resistance
-            gas_offset = gas_baseline - gas
 
             hum = sensor.data.humidity
-            hum_offset = hum - hum_baseline
-
-            # Calculate hum_score as the distance from the hum_baseline.
-            if hum_offset > 0:
-                hum_score = (100 - hum_baseline - hum_offset) / (100 - hum_baseline) * (hum_weighting * 100)
-
-            else:
-                hum_score = (hum_baseline + hum_offset) / hum_baseline * (hum_weighting * 100)
-
-            # Calculate gas_score as the distance from the gas_baseline.
-            if gas_offset > 0:
-                gas_score = (gas / gas_baseline) * (100 - (hum_weighting * 100))
-
-            else:
-                gas_score = 100 - (hum_weighting * 100)
-
-            # Calculate air_quality_score.
-            air_quality_score = hum_score + gas_score
-
-#        sensor.get_sensor_data()
             temp_score = sensor.data.temperature
             press_score = sensor.data.pressure
-#        hum_score = '{:.1f}'.format(sensor.data.humidity)
-#        air_quality_score = 100
+
 
     except:
         hum = 0
